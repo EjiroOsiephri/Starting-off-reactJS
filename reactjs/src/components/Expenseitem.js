ab3efbbef58ffb570de9decb07609b41a88bdd45
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-03-02 08:54:08 +0100

    Events listener

--- reactjs/src/components/Expenseitem.js
+++ reactjs/src/components/Expenseitem.js
@@ -2,11 +2,15 @@ import "./Expense.css";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("clicked!");
+  };
   return (
     <div className="expense">
       <ExpenseDate date={props.date} />
       <h2>{props.title}</h2>
       <h3 className="expense-item">${props.item}</h3>
+      <button onClick={clickHandler}>Click me</button>
     </div>
   );
 }
